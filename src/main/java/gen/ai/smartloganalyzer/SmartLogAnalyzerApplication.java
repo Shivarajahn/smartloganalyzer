d03e2487a2aa3a530d84449687a46a55b6f323b0
Hn <shivaraja.hn@accenture.com>
2025-11-20 21:13:32 +0530

    initial commit

---
+++ src/main/java/gen/ai/smartloganalyzer/SmartLogAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package gen.ai.smartloganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLogAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gen/ai/smartloganalyzer/SmartLogAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package gen.ai.smartloganalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
